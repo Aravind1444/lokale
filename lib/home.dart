d048b284010859133f9d9e5b766bd827e6eae435
Aravind Venugopal <aravindvenugopal02@gmail.com>
2020-06-28 17:11:58 +0530

    worked on home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -15,7 +15,56 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(66, 230, 180, 1),
+      ),
+      endDrawer: Drawer(),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 200,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [Color.fromRGBO(37, 124, 162, 1), Color.fromRGBO(66, 230, 180, 0.5)])),
+              child: Stack(
+                children: <Widget>[
+
+                  Positioned(
+                    bottom: 0,
+                    left: 10,
+                    child: Image(
+                      image: AssetImage(
+                        'images/Compass1.png',
+                      ),),
+                  ),
+
+                  Positioned(
+                    bottom: 50,
+                    right: 20,
+                    child: Image(
+                      image: AssetImage(
+                        'images/sphere1.png',
+                      ),),
+                  ),
+
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Image(
+                      image: AssetImage(
+                        'images/sphere2.png',
+                      ),),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
